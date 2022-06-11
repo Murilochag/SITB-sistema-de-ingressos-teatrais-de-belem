51a4434b7e89a1eaf2c749812bc1240da990007d
Murilo <murilochagas10@outlook.com>
2022-06-10 22:44:30 -0300

    cadastro novo show no banco de dados

--- SITB/main.py
+++ SITB/main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import sqlite3
-import services.showdb as showdb
+import data.showdb as showdb
+import pandas as pd
 
 ##st.title('SITB-sistema de ingressos teatrais de belém')
 st.sidebar.title('menu')
@@ -16,17 +17,33 @@ elif page == 'compras' :
     st.title('compras')
 elif page == 'comprar ingresso' :
     st.title('comprar ingresso')
+
+
 elif page == 'shows' :
     st.title('shows')
+    costumerList = []
+
+    for item in showdb.selecionarTodos():
+        costumerList.append(item.id, item.name, item.lacation, item.description)
+
+    df = pd.DataFrame(
+        costumerList = ['id','nome','local','descrição']
+    )
+
+    st.table(df)
+
 #novo show
 elif page == 'novo show' :
     st.title('novo show')
 
     with st.form(key="new_show"):
-        showid = st.text_input(label='insira o id')
         showname = st.text_input(label='insira o nome')
-        showlocation = st.selectbox('selecione o teatro',['teatro da paz','margarida saquivasapa e tal','teatro do sesi'])
+        showlocation = st.selectbox('selecione o teatro',['teatro da paz','estação gasõmestro','waldemar henrique','teatro do sesi','margarida schivasappa'])
+        showdescription = st.text_area(label='descrição')
+
         input_button_submit = st.form_submit_button('cadastar novo show')
 
     if input_button_submit :
-        showdb.insertInShow(showid,showname,showlocation)
+        showid = showdb.idShow()
+        showdb.insertInShow(showid,showname,showlocation,showdescription)
+        st.success('Show cadastrado com sucesso!')
